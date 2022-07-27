cea57d9a820eeb6c65fae7d8c1dc5bc8b1428a98
darkburner01 <junior_cwf@hotmail.com>
2022-07-27 11:40:05 +0800

    new

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,5 +1,15 @@
-function ExpenseItem() {
-    return <h>Expense Item!</h>
+import './ExpenseItem.css';
 
+
+function ExpenseItem() {
+  return (
+    <div className='expense-item'>
+      <div>Date</div>
+      <div className='expense-item_description'>
+        <h2>Title</h2>
+      </div>
+      <div className='expense-item_price'>Amount</div>
+    </div>
+  );
 }
 export default ExpenseItem;
